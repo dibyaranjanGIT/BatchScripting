6cda27a8931f368e9f494e4ff9ba75fdd8be49cf
Dibyaranjan Jena <45364252+dibyaranjanGIT@users.noreply.github.com>
2022-03-21 23:35:29 +0530

    Add files via upload

---
+++ CommandLineArg.bat
@@ -0,0 +1,10 @@
+@echo off
+
+echo Command line argument
+
+echo The first argument is : %1 || This for first Argument
+echo The second argument is : %2 || This for Second Argument
+
+pause
+
+|| Run this file from your CLI
